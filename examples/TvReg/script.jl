85cf9f7001ddadd685420e40fd85b1fa59c98513
mv <mattias.villani@gmail.com>
2024-12-06 16:52:26 +0100

    tvReg example

--- examples/TvReg/script.jl
+++ examples/TvReg/script.jl
@@ -1,27 +1,48 @@
-# Simulates from the posterior in the time-varying regression model:
-# yₜ = z_t'βₜ + εₜ, εₜ ∼ N(0,σₑ)
-# βₜ = βₜ₋₁ + νₜ, νₜ ∼ N(0,Q), with Q constant over time
-# β₀ ~ N(0, Σ₀)
+# # Time-varying regression model
 
-using SMCsamplers, Plots, Distributions, LaTeXStrings, Random, PDMats
-using LinearAlgebra
+# In this example we explore the joint posterior of the state $\beta_t$ in a time-varying regression model with known static parameters: 
+# ```math
+# \begin{align*}
+#   y_t &= z_t^\top\beta_t + \varepsilon_t,  \quad \varepsilon_t \sim N(0,\sigma_\varepsilon) \\
+#   \beta_t &= \beta_{t-1} + \nu_t, \quad \nu_t \sim N(0,Sigma_\eta) \\
+#   \beta_0 &\sim N(0, \Sigma_0)  
+# \end{align*}
+# ```
 
-include("ExampleModels.jl") # exports SimTVReg to simulate from the model
+# Preminiaries
 
-# Plot settings
+using SMCsamplers, Plots, Distributions, LaTeXStrings, Random, PDMats, LinearAlgebra
+
 gr(legend = :topleft, grid = false, color = colors[2], lw = 2, legendfontsize=8,
     xtickfontsize=8, ytickfontsize=8, xguidefontsize=8, yguidefontsize=8,
     titlefontsize = 10, markerstrokecolor = :auto)
 
 myquantile(A, p; dims, kwargs...) = mapslices(x -> quantile(x, p; kwargs...), A; dims)
-Random.seed!(123)
+Random.seed!(123);
 
-# Simulate some data
+# ### Simulate time-varying regression data
+function SimTVReg(T, p, σₑ, Σₙ, Σ₀ = Σₙ)     
+
+    Z = randn(T, p) # Matrix with covariates
+    y = zeros(T)
+    β = zeros(T, p)
+    for t in 1:T
+        if t == 1
+            β₀ = rand(MvNormal(zeros(p), Σ₀))
+            β[t,:] = β₀ + rand(MvNormal(Σₙ))
+        else
+            β[t,:] = β[t-1,:] + rand(MvNormal(Σₙ))
+        end
+        y[t] = Z[t,:] ⋅ β[t,:] + rand(Normal(0, σₑ))
+    end
+    return y, Z, β
+end
+
 p = 2      # State size - number of β parameters, including intercept
 T = 200    # Number of observations
 σₑ = 1
 Σₙ = PDMat([1 0;0 0.1])     # State innovation covariance matrix
-y, Z, β, Q = SimTVReg(T, p, σₑ, Σₙ)
+y, Z, β = SimTVReg(T, p, σₑ, Σₙ);
 
 # Plot the parameter evolution
 plt = plot()
@@ -30,7 +51,7 @@ for j in 1:p
 end
 plt
 
-# Set up time-varying regression models
+# ### Set up time-varying regression models
 mutable struct ParamTvReg
     σₑ::Float64
     Σₙ::PDMat{Float64}
@@ -45,18 +66,16 @@ observation(θ, state, t) = Normal(θ.Z[t,:] ⋅ state, θ.σₑ)
 
 μ₀ = zeros(p)
 Σ₀ = PDMat(10*I(p))
-θ = ParamTvReg(σₑ, Σₙ, μ₀, Σ₀, y, Z)
+θ = ParamTvReg(σₑ, Σₙ, μ₀, Σ₀, y, Z);
 
-# Algorithm settings
+# ### PGAS
 Nₚ = 20       # Number of particles
 Nₛ = 1000     # Number of samples from posterior
-
-println("Generating $Nₛ PGAS draws based on $Nₚ particles")
-@time PGASdraws = PGASsampler(y, θ, Nₛ, Nₚ, prior, transition, observation)
+PGASdraws = PGASsampler(y, θ, Nₛ, Nₚ, prior, transition, observation)
 PGASmean = mean(PGASdraws, dims = 3)[:,:,1]
-PGASquantiles = myquantile(PGASdraws, [0.025, 0.975], dims = 3)
+PGASquantiles = myquantile(PGASdraws, [0.025, 0.975], dims = 3);
 
-## FFBS
+# ### FFBS
 Σₑ = σₑ^2
 A = collect(I(p))
 C = zeros(1,p,T)
@@ -66,29 +85,28 @@ end
 B = 0.0
 U = zeros(T,1)
 
-println("Generating $Nₛ FFBS draws")
-@time FFBSdraws = FFBS(U, y, A, B, C, Σₑ, Σₙ, μ₀, Σ₀, Nₛ);
+FFBSdraws = FFBS(U, y, A, B, C, Σₑ, Σₙ, μ₀, Σ₀, Nₛ);
 FFBSmean = mean(FFBSdraws, dims = 3)[2:end,:,1] # Exclude initial state at t=0
-FFBSquantiles = myquantile(FFBSdraws, [0.025, 0.975], dims = 3)[2:end,:,:]
+FFBSquantiles = myquantile(FFBSdraws, [0.025, 0.975], dims = 3)[2:end,:,:];
 
-plottrue = false
+# ### Plot the posterior mean and 95% credible intervals from both algorithms
+plottrue = true
 p = length(prior(θ))
 plt = []
 for j in 1:p
-    # True state evolution
+    ## True state evolution
     if plottrue
-        plt_tmp = plot(x, c = :gray, lw = 1, title = L"\beta_{%$(j-1)}", 
+        plt_tmp = plot(β[:,j], c = colors[3], lw = 1, title = L"\beta_{%$(j-1)}", 
             label = "true state", legend = :bottomleft)
     else
         plt_tmp = plot(title = L"\beta_{%$(j-1)}", legend = :bottomleft)
     end
-    # PGAS
-    plot!(PGASmean[:,j], lw = 1,
-        c = colors[j], linestyle = :solid, label = "PGAS(N=$Nₚ)")
+    ## PGAS
+    plot!(PGASmean[:,j], lw = 1, c = colors[j], linestyle = :solid, label = "PGAS(N=$Nₚ)")
     plot!(PGASquantiles[:,j,1], fillrange = PGASquantiles[:,j,2],
         fillalpha = 0.2, fillcolor = colors[j], linecolor = colors[j],
         label = "", lw = 0) 
-    # FFBS
+    ## FFBS
     plot!(FFBSmean[:,j]; color = :black, lw = 1, linestyle = :dash, label="FFBS")
     plot!(FFBSquantiles[:,j,1], lw = 1, c = :black, linestyle = :dash, label="")
     plot!(FFBSquantiles[:,j,2], lw = 1, c = :black, linestyle = :dash, label = "")
@@ -96,3 +114,5 @@ for j in 1:p
     push!(plt, plt_tmp)
 end
 plot(plt..., layout = (1,p), size = (800,300))
+
+# The posterior mean and 95% credible intervals from both algorithms are indeed almost identical.
